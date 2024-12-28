7f5271875a76ceaf58146d69789ac5f2347a006a
DAO Hamadou <daohamadou@gmail.com>
2024-12-28 20:01:23 +0000

    -

--- android/src/main/java/com/lina/smsplugin/smsplugin.java
+++ android/src/main/java/com/lina/smsplugin/smsplugin.java
@@ -1,11 +1,40 @@
 package com.lina.smsplugin;
 
-import android.util.Log;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.JSObject;
 
-public class smsplugin {
+@CapacitorPlugin(name = "SmsPlugin")
+public class SmsPlugin extends Plugin {
+    public static class SmsReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if ("android.provider.Telephony.SMS_RECEIVED".equals(intent.getAction())) {
+                Bundle bundle = intent.getExtras();
+                if (bundle != null) {
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    if (pdus != null) {
+                        for (Object pdu : pdus) {
+                            SmsMessage sms = SmsMessage.createFromPdu((byte[]) pdu);
+                            String sender = sms.getDisplayOriginatingAddress();
+                            String messageBody = sms.getMessageBody();
 
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
+                            JSObject data = new JSObject();
+                            data.put("sender", sender);
+                            data.put("message", messageBody);
+
+                            // Émettez un événement vers le front-end
+                            SmsPlugin plugin = SmsPlugin.this;
+                            plugin.notifyListeners("onSmsReceived", data);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ android/src/main/java/com/lina/smsplugin/smspluginTest.java
@@ -0,0 +1,11 @@
+package com.lina.smsplugin;
+
+import android.util.Log;
+
+public class smsplugin {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
